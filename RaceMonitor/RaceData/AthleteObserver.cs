8198fa74d4b06502dc72aa6db76153362c1a62d1
Parker Petersen <parkpetersen@gmail.com>
2017-09-25 16:33:21 -0600

    beginning work

---
+++ RaceMonitor/RaceData/AthleteObserver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RaceData
+{
+    class AthleteObserver : Form
+    {
+        private Dictionary<int, Athlete> ObservedAthletes = new Dictionary<int, Athlete>();
+
+        public void Update(Subject subject)
+        {
+
+        }
+    }
+}
